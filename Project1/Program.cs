ae37db1855fd6e454aa08e16848ac7347f5bb1f7
Zayon Odinson <lrusso.x@gmail.com>
2018-09-16 18:05:51 -0300

    criado o primeiro project, aula 1

---
+++ Project1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1,2,3,4,5,6,7,8,9,10");
+
+            for(var s = 1; s <= 1000;s=s+1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.Read();
+        }
+
+    }
+}
